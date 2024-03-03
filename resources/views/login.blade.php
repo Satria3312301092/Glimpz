d0554f6e6698aee2fc78b779eebfbab64d70e047
michael3312301112 <liong888g@gmail.com>
2024-03-03 21:17:33 +0700

    Update PBL4

--- resources/views/login.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-    <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
-    <h3><b>Login</b></h3>
-    <label for="">Username</label><br>
-    <input type="text"><br>
-    <label for="">Password</label><br>
-    <input type="password"><br>
-    <button type="Submit">Login</button>
-</div>
