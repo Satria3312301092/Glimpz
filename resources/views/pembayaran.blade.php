a918b2334034ee6031dc34e08289331448a9225f
Jamal-crypto-coin <richkyahnmad@gmail.com>
2024-03-14 09:22:28 +0700

    P5_PW

--- resources/views/pembayaran.blade.php
+++ resources/views/pembayaran.blade.php
@@ -1,4 +1,17 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('Jamal-CSS/style.css') }}">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="bg-orange-500 h-screen">
+    <div class="mx-8 ">
+
+<h1>Pembayaran</h1>
     <table>
         <thead>
             <tr>
@@ -19,4 +32,10 @@
             </tbody>
         </thead>
     </table>
+    <img class="image1" src="Jamal-Image/header.jpg">
+</div> 
+</div>
+     
+
+</body>
 </html>
