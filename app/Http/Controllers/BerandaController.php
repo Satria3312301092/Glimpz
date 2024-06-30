a31b6741ea9b62d985be9d59000ae234385464d1
michael3312301112 <liong888g@gmail.com>
2024-06-30 17:39:49 +0700

    Co-authored-by: mhdadji1703@gmail.com <mhdadji1703@gmail.com> Co-authored-by: mrfarhanramadhan23@gmail.com <mrfarhanramadhan23@gmail.com>

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -8,16 +8,17 @@ use Illuminate\Http\Request;
 
 class BerandaController extends Controller
  {
-        public function beranda(Request $request){
-            $users = Auth::user();
-            $category = $request->input('category');
-
-            if($category) {
-                $services = Service::where('Category', $category)->get();
-            } else {
-                $services = Service::all();
-            }
-
-            return view('beranda', compact('users','category'));
+    public function beranda(){
+        $users = Auth::user();
+        $services = Service::take(8)->get();
+        $types = Type::all();
+        $details = Detail::all();
+    
+        foreach ($services as $service) {
+            $service->average_rating = $service->averageRating();
+            $service->rating_count = $service->ratingCount();
         }
+        
+        return view("beranda", compact('users','services','types','details'));    
     }
+}
